68155e01c20ad6c37ea8386df6a35fe22053da12
Mane087 <96441938+Mane087@users.noreply.github.com>
2025-02-01 18:36:17 -0600

    Update nodeMailer.js

--- src/middleware/nodeMailer.js
+++ src/middleware/nodeMailer.js
@@ -7,9 +7,9 @@ class MailService {
       auth: {
         type: 'OAuth2',
         user: 'manealaniz95@gmail.com',
-        clientId: '243138539675-do4rc21rduc6o0uts6ubtn464af4rdeg.apps.googleusercontent.com',
-        clientSecret: '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',
-        refreshToken: '1//04pQjnW_r-J7gCgYIARAAGAQSNwF-L9IryFjm3I-Sp7lH4FcQ0QH0e-w8Umz5o_Dgj2x1EWedrGhT9kZmQxx4JTLn4eNgZmFuDhE'
+        clientId: '--',
+        clientSecret: '--',
+        refreshToken: '---'
       }
     });
   }
